b77791746e4c723c3841d6ee67e6692e972be60f
felixNyalenda <felixasejunior@gmail.com>
2023-06-10 14:05:35 +0300

    footer add social links & align subs section

--- src/APP/components/Footer.jsx
+++ src/APP/components/Footer.jsx
@@ -1,3 +1,9 @@
+import {
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+} from "../../assets/images/socials";
 import logo from "../../assets/images/sytLogo.png";
 
 function Footer() {
@@ -6,14 +12,35 @@ function Footer() {
       <div className=" bg-[#252533] mx-auto w-full max-w-screen-2xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
           {/* logo and socials  */}
-          <div className="my-6 md:mb-0">
-            <img src={logo} className="h-24 mx-8" alt="FlowBite Logo" />
+          <div className="mb-8 mr-6 md:mb-0 flex flex-col justify-start items-center w-1/2">
+            <img
+              src={logo}
+              className="h-24 md:mx-8 mx-0 object-contain"
+              alt="FlowBite Logo"
+            />
             {/* social logos  */}
+            <div className="flex flex-row items-center  md:justify-between gap-4 justify-start my-4">
+              <a href="/">
+                <img src={facebook} alt="facebook" className="w-7 h-7" />
+              </a>
+
+              <a href="/">
+                <img src={instagram} alt="instagram" className="w-7 h-7" />
+              </a>
+
+              <a href="/">
+                <img src={twitter} alt="twitter" className="w-7 h-7" />
+              </a>
+
+              <a href="/">
+                <img src={linkedin} alt="linkedIn" className="w-7 h-7" />
+              </a>
+            </div>
           </div>
           {/* quick links  */}
           <div className="grid grid-cols-3 gap-8 sm:gap-6 sm:grid-cols-4">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-[#00FFC3] uppercase">
+              <h2 className="mb-6 text-sm font-semibold text-[#00FFC3] ">
                 Company
               </h2>
               <ul className="text-gray-600 dark:text-gray-400 font-medium">
@@ -44,7 +71,7 @@ function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-[#00FFC3] uppercase">
+              <h2 className="mb-6 text-sm font-semibold text-[#00FFC3] ">
                 Resources
               </h2>
               <ul className="text-gray-600 dark:text-gray-400 font-medium">
@@ -75,7 +102,7 @@ function Footer() {
               </ul>
             </div>
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-[#00FFC3] uppercase">
+              <h2 className="mb-6 text-sm font-semibold text-[#00FFC3] ">
                 Quick Links
               </h2>
               <ul className="text-gray-600 dark:text-gray-400 font-medium">
@@ -115,12 +142,12 @@ function Footer() {
             </div>
           </div>
           {/* form  */}
-          <div className="my-6 md:mb-0">
+          <div className="my-0 md:mb-0 flex flex-col">
             <div>
-              <h3 className="text-xs font-semibold tracking-wider text-[#00FFC3] uppercase">
+              <h3 className="text-sm font-semibold tracking-wider text-[#00FFC3] ">
                 Subscribe to our newsletter
               </h3>
-              <p className="mt-4 text-sm  text-white lg:ml-auto">
+              <p className="mt-4 text-base text-white lg:ml-auto">
                 Subscribe to our newsletter to get your weekly dose of news,
                 updates, tips and even special offers
               </p>
@@ -129,67 +156,32 @@ function Footer() {
               className="
       relative
       my-4
-      mx-auto
       max-w-lg
       space-y-4
       sm:flex sm:space-y-0 sm:items-end
     "
             >
-              <div className="flex-1">
-                <label htmlFor="email" className="sr-only">
+              <div className="flex-1 flex items-center">
+                {/* <label htmlFor="email" className="sr-only">
                   Email address
-                </label>
-                <div>
-                  <input
-                    type="email"
-                    id="email"
-                    autoComplete="off"
-                    className="
-            block
-            w-full
-            px-4
-            py-3
-            sm:py-3.5
-            text-base
-            font-medium
-            text-gray-900
-            placeholder-gray-500
-            border border-gray-300
-            rounded-lg
-            sm:rounded-l-lg sm:rounded-r-none sm:text-sm
-            focus:outline-none focus:ring-1 focus:ring-primary
-          "
-                    placeholder="Enter email address"
-                  />
-                </div>
+                </label> */}
+                {/* <div> */}
+                <input
+                  type="email"
+                  id="email"
+                  autoComplete="off"
+                  className="sm:flex-4 flex-auto block sm:w-2/3 w-full px-4 py-3 h-12 sm:py-3.5 text-base font-medium text-gray-900 placeholder-gray-500 border border-gray-300 rounded-lg sm:rounded-l-lg rounded-r-none sm:text-sm focus:outline-none focus:ring-1 focus:ring-primary"
+                  placeholder="Enter email address"
+                />
+
+                <button
+                  type="submit"
+                  className="sm:flex-1 flex-auto inline-flex items-center justify-center w-1/3 sm:w-auto px-8 py-3 h-12 sm:text-sm text-lg sm:py-3.5 font-semibold text-white transition-all duration-200 bg-[#009975] border border-transparent rounded-lg sm:rounded-r-lg rounded-l-none hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                >
+                  Subscribe
+                </button>
               </div>
-              <button
-                type="submit"
-                className="inline-flex items-center
-        justify-center
-        w-full
-        sm:w-auto
-        px-8
-        py-3
-        sm:text-sm
-        text-lg
-        sm:py-3.5
-        font-semibold
-        text-white
-        transition-all
-        duration-200
-        bg-[#009975]
-        border border-transparent
-        rounded-lg
-        sm:rounded-r-lg sm:rounded-l-none
-        hover:bg-gray-700
-        focus:outline-none
-        focus:ring-2
-        focus:ring-offset-2
-        focus:ring-gray-900"
-              >
-                Subscribe
-              </button>
+              {/* </div> */}
             </form>
           </div>
         </div>
---
+++ src/assets/images/socials/index.js
@@ -0,0 +1,6 @@
+import linkedin from "./linkedIn.svg";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import instagram from "./instagram.svg";
+
+export { linkedin, facebook, twitter, instagram };
