0c094acad0991069048d0f43623900f9cedaf419
felixNyalenda <felixasejunior@gmail.com>
2023-06-10 12:27:31 +0300

    cta background insertion

--- src/APP/pages/landingPage/sections/CTASection.jsx
+++ src/APP/pages/landingPage/sections/CTASection.jsx
@@ -1,12 +1,12 @@
 function CTASection() {
   return (
-    <section className="text-gray-600 body-font">
+    <section className="text-gray-600 body-font relative">
       <div className="container px-5 py-12 mx-auto">
         <div className="flex flex-col text-center w-full mb-8 md:mb-12">
-          <h1 className="sm:text-3xl text-3xl font-medium title-font mb-4 text-[#00664E]">
+          <h1 className="sm:text-3xl text-3xl font-semibold title-font mb-4 text-[#00664E]">
             Join, thrive, grow
           </h1>
-          <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-base font-normal">
             You miss all the shots you do not take. Invest your time in horning
             your craft.
           </p>
@@ -15,6 +15,14 @@ function CTASection() {
           <button className="inline-flex  text-white bg-[#009975] border-0 py-3 px-8 focus:outline-none  rounded-lg text-lg">
             Join the community
           </button>
+        </div>
+      </div>
+
+      <div className="w-full h-full absolute top-0 left-0">
+        <div className="relative">
+          <div className="sm:blur-1 blur-sm-1">ase</div>
+          <div className="sm:blur-2 blur-sm-2">ase</div>
+          <div className="sm:blur-3 blur-sm-3">ase</div>
         </div>
       </div>
     </section>
