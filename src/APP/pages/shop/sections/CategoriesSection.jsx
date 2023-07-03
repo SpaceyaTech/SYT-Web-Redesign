cf8931e8daad7cecfb623f13ee1793fb0909784c
DevHumbleChris <dev.humblechris254@gmail.com>
2023-07-03 15:40:54 +0300

    added products and categories

--- src/APP/pages/shop/sections/CategoriesSection.jsx
+++ src/APP/pages/shop/sections/CategoriesSection.jsx
@@ -1,5 +1,73 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+
 function CategoriesSection() {
-  return <div>CategoriesSection</div>;
+  const [categories, setCategories] = useState([
+    {
+      id: 1,
+      name: "Hoodies",
+      imgURL:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+    },
+    {
+      id: 2,
+      name: "T-Shirts",
+      imgURL:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+    },
+    {
+      id: 3,
+      name: "Stickers",
+      imgURL:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+    },
+    {
+      id: 4,
+      name: "Cups",
+      imgURL:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+    },
+    {
+      id: 5,
+      name: "Bookmarks",
+      imgURL:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+    },
+    {
+      id: 6,
+      name: "Hoodies",
+      imgURL:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+    },
+  ]);
+  return (
+    <>
+      <div className="p-10">
+        <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+          Categories
+        </h2>
+        <div className="grid grid-cols-3 gap-x-8 p-3">
+          {categories.map((category) => {
+            return (
+              <>
+                <div key={category.id}>
+                  <img
+                    src={category.imgURL}
+                    className="object-cover object-center rounded-xl"
+                    alt=""
+                  />
+                  <div className="p-2 flex items-center justify-between">
+                    <p>{category.name}</p>
+                    <Link className="text-[#009975] text-sm">Learn more</Link>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CategoriesSection;
--- src/APP/pages/shop/sections/PopularItemsSection.jsx
+++ src/APP/pages/shop/sections/PopularItemsSection.jsx
@@ -1,5 +1,83 @@
+import { useState } from "react";
+
 function PopularItemsSection() {
-  return <div>PopularItemsSection</div>;
+  const [products, setProducts] = useState([
+    {
+      id: 1,
+      name: "Bookmarks",
+      href: "#",
+      imageSrc:
+        "https://images.unsplash.com/photo-1588618575327-87bfc763efd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=865&q=80",
+      price: "Ksh 500",
+    },
+    {
+      id: 2,
+      name: "SpaceYaTech Hoodie",
+      href: "#",
+      imageSrc:
+        "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80",
+      price: "Ksh 500",
+    },
+    {
+      id: 3,
+      name: "Mentorist T-shirt",
+      href: "#",
+      imageSrc:
+        "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=436&q=80",
+      price: "Ksh 500",
+    },
+    {
+      id: 4,
+      name: "SpaceYaTech Mug",
+      href: "#",
+      imageSrc:
+        "https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1936&q=80",
+      price: "Ksh 500",
+    },
+  ]);
+  return (
+    <>
+      <div>
+        <div class="p-10">
+          <h2 class="text-2xl font-bold tracking-tight text-gray-900">
+            Popular items from SpaceYaTech
+          </h2>
+
+          <div class="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+            {products.map((product) => {
+              return (
+                <>
+                  <div key={product.id} class="group relative">
+                    <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src={product.imageSrc}
+                        alt="Front of men&#039;s Basic Tee in black."
+                        class="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div class="mt-4 flex justify-between">
+                      <div className="space-y-2">
+                        <h3 class="text-sm text-gray-700">
+                          <a href="#">
+                            <span
+                              aria-hidden="true"
+                              class="absolute inset-0"
+                            ></span>
+                            { product.name }
+                          </a>
+                        </h3>
+                        <p class="text-sm font-medium text-gray-900">{ product.price }</p>
+                      </div>
+                    </div>
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default PopularItemsSection;
