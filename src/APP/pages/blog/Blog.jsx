a6c1114d6bd284687b4125a877e8db13dc4f44a4
felixNyalenda <felixasejunior@gmail.com>
2023-08-21 03:18:13 +0300

    search blog

--- src/APP/pages/blog/Blog.jsx
+++ src/APP/pages/blog/Blog.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+
 import Banner from "./sections/Banner";
 import BlogsWrapper from "./sections/BlogsWrapper";
 
 const Blog = () => {
+  const [searchText, setSearchText] = useState("");
+
   return (
     <section className="flex flex-col items-center gap-4">
-      <Banner />
-      <BlogsWrapper />
+      <Banner searchText={searchText} setSearchText={setSearchText} />
+      <BlogsWrapper searchText={searchText} />
     </section>
   );
 };
--- src/APP/pages/blog/sections/BlogsWrapper.jsx
+++ src/APP/pages/blog/sections/BlogsWrapper.jsx
@@ -1,19 +1,33 @@
 import { blogCard, blogCat } from "../data";
 import BlogCard from "./BlogCard";
 
-const BlogsWrapper = () => {
+const FilterBtns = () => {
+  return (
+    <div className="w-max overflow-scroll md:overflow-auto flex flex-row items-center gap-3">
+      {blogCat.map((blog) => (
+        <span
+          key={blog.id}
+          className="bg-gray-100 text-black text-sm py-1 px-3 rounded-2xl cursor-pointer transition-all duration-500 ease-in hover:bg-[#009975] hover:text-white active:bg-[#009975] active:text-white w-fit whitespace-normal"
+        >
+          {blog.cat}
+        </span>
+      ))}
+    </div>
+  );
+};
+
+const SearchResults = ({ searchText }) => {
+  return (
+    <h3 className="text-black text-xl md:text-3xl font-semibold leading-8 md:leading-loose text-center">
+      Showing results for “{searchText}”
+    </h3>
+  );
+};
+
+const BlogsWrapper = ({ searchText }) => {
   return (
     <div className="flex flex-col items-start md:items-center gap-8 md:gap-16 px-4 pt-4 xl:px-14 w-full mb-10 md:mb-40">
-      <div className="w-max overflow-scroll md:overflow-auto flex flex-row items-center gap-3">
-        {blogCat.map((blog) => (
-          <span
-            key={blog.id}
-            className="bg-gray-100 text-black text-sm py-1 px-3 rounded-2xl cursor-pointer transition-all duration-500 ease-in hover:bg-[#009975] hover:text-white active:bg-[#009975] active:text-white w-fit whitespace-normal"
-          >
-            {blog.cat}
-          </span>
-        ))}
-      </div>
+      {searchText ? <SearchResults searchText={searchText} /> : <FilterBtns />}
       <div className="grid sm:grid-cols-2  gap-16 grid-cols-1">
         {blogCard.map((blog) => (
           <BlogCard key={blog.id} blog={blog} />
