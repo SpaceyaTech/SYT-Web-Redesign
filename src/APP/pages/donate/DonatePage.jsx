d279e81a0361ad5deb32662c5c02c29f4dc44854
DevHumbleChris <dev.humblechris254@gmail.com>
2023-08-22 18:05:34 +0300

    finished designing the donate page

--- src/APP/pages/donate/DonatePage.jsx
+++ src/APP/pages/donate/DonatePage.jsx
@@ -1,53 +1,53 @@
-import React from "react";
-import RangeInput from "./RangeInput";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import RangeInput from './RangeInput';
 
 // this is the donate page with products and donation progress bar
 function DonatePage() {
   const projects = [
     {
       id: 1,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 2,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 3,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 4,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 5,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 6,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 7,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
     {
       id: 8,
-      name: "Mastori CMS Platform",
-      info: "A standalone content management platform for tech blogs and news where everyone contributes.",
+      name: 'Mastori CMS Platform',
+      info: 'A standalone content management platform for tech blogs and news where everyone contributes.',
     },
   ];
   return (
-    <section className="my-2 space-y-12">
+    <section className="my-2 space-y-12 p-3 mb-10">
       <div className="max-w-3xl mx-auto text-center space-y-6">
         <h2 className="text-4xl">Promote a Projet</h2>
         <p>
@@ -76,50 +76,51 @@ function DonatePage() {
         </div>
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-4 gap-4">
-        {projects?.map((project) => {
-          return (
-            <div key={project.id} className="shadow-lg rounded-md text-sm">
-              <div className="h-[12rem]">
-                <img
-                  src="https://images.unsplash.com/photo-1569098644584-210bcd375b59?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-                  alt="unsplash-img"
-                  className="object-cover w-full h-full rounded-t-md"
-                />
-              </div>
-              <div className="p-2">
-                <div className="flex items-center justify-between my-2">
-                  <h3 className="font-semibold">Mastori CMS Platform</h3>
-                  <p className="text-[#0061CC] bg-[#E8F3FF] px-2 p-1 rounded-full">
-                    Active
-                  </p>
-                </div>
-                <p className="text-gray-600 mb-5">
-                  A standalone content management platform for tech blogs and
-                  news where everyone contributes.
+        {projects?.map((project) => (
+          <div key={project.id} className="shadow-lg rounded-md text-sm">
+            <div className="h-[12rem]">
+              <img
+                src="https://images.unsplash.com/photo-1569098644584-210bcd375b59?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
+                alt="unsplash-img"
+                className="object-cover w-full h-full rounded-t-md"
+              />
+            </div>
+            <div className="p-2">
+              <div className="flex items-center justify-between my-2">
+                <h3 className="font-semibold">Mastori CMS Platform</h3>
+                <p className="text-[#0061CC] bg-[#E8F3FF] px-2 p-1 rounded-full">
+                  Active
                 </p>
-                <RangeInput />
-                <div className="flex items-center justify-between my-2">
-                  <img src="/donate.png" alt="donate-img" />
-                  <Link to={`/donate/${project?.id}`} className="flex items-center gap-2">
-                    <p className="text-[#009975]">View Project</p>
-                    <svg
-                      width="16"
-                      height="14"
-                      viewBox="0 0 16 14"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M9.69219 0.932465L15.3172 6.55746C15.3753 6.61551 15.4214 6.68444 15.4529 6.76031C15.4843 6.83619 15.5005 6.91752 15.5005 6.99965C15.5005 7.08179 15.4843 7.16312 15.4529 7.23899C15.4214 7.31486 15.3753 7.38379 15.3172 7.44184L9.69219 13.0668C9.57491 13.1841 9.41585 13.25 9.25 13.25C9.08415 13.25 8.92509 13.1841 8.80781 13.0668C8.69054 12.9496 8.62465 12.7905 8.62465 12.6247C8.62465 12.4588 8.69054 12.2997 8.80781 12.1825L13.3664 7.62465L1.125 7.62465C0.95924 7.62465 0.800269 7.5588 0.683058 7.44159C0.565848 7.32438 0.5 7.16541 0.5 6.99965C0.5 6.83389 0.565848 6.67492 0.683058 6.55771C0.800269 6.4405 0.95924 6.37465 1.125 6.37465L13.3664 6.37465L8.80781 1.81684C8.69054 1.69956 8.62465 1.5405 8.62465 1.37465C8.62465 1.2088 8.69054 1.04974 8.80781 0.932465C8.92509 0.815188 9.08415 0.749304 9.25 0.749304C9.41585 0.749304 9.57491 0.815188 9.69219 0.932465Z"
-                        fill="#009975"
-                      />
-                    </svg>
-                  </Link>
-                </div>
+              </div>
+              <p className="text-gray-600 mb-5">
+                A standalone content management platform for tech blogs and
+                news where everyone contributes.
+              </p>
+              <RangeInput />
+              <div className="flex items-center justify-between my-2">
+                <img src="/donate.png" alt="donate-img" />
+                <Link
+                  to={`/donate/${project?.id}`}
+                  className="flex items-center gap-2"
+                >
+                  <p className="text-[#009975]">View Project</p>
+                  <svg
+                    width="16"
+                    height="14"
+                    viewBox="0 0 16 14"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M9.69219 0.932465L15.3172 6.55746C15.3753 6.61551 15.4214 6.68444 15.4529 6.76031C15.4843 6.83619 15.5005 6.91752 15.5005 6.99965C15.5005 7.08179 15.4843 7.16312 15.4529 7.23899C15.4214 7.31486 15.3753 7.38379 15.3172 7.44184L9.69219 13.0668C9.57491 13.1841 9.41585 13.25 9.25 13.25C9.08415 13.25 8.92509 13.1841 8.80781 13.0668C8.69054 12.9496 8.62465 12.7905 8.62465 12.6247C8.62465 12.4588 8.69054 12.2997 8.80781 12.1825L13.3664 7.62465L1.125 7.62465C0.95924 7.62465 0.800269 7.5588 0.683058 7.44159C0.565848 7.32438 0.5 7.16541 0.5 6.99965C0.5 6.83389 0.565848 6.67492 0.683058 6.55771C0.800269 6.4405 0.95924 6.37465 1.125 6.37465L13.3664 6.37465L8.80781 1.81684C8.69054 1.69956 8.62465 1.5405 8.62465 1.37465C8.62465 1.2088 8.69054 1.04974 8.80781 0.932465C8.92509 0.815188 9.08415 0.749304 9.25 0.749304C9.41585 0.749304 9.57491 0.815188 9.69219 0.932465Z"
+                      fill="#009975"
+                    />
+                  </svg>
+                </Link>
               </div>
             </div>
-          );
-        })}
+          </div>
+        ))}
       </div>
     </section>
   );
