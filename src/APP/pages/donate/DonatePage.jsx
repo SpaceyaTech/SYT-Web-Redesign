da0dc0cecf022032977c5668d069532aa5724dc1
DevHumbleChris <dev.humblechris254@gmail.com>
2023-08-22 18:00:54 +0300

    finished designing the donate page

--- src/APP/pages/donate/DonatePage.jsx
+++ src/APP/pages/donate/DonatePage.jsx
@@ -1,20 +1,9 @@
-import React, { useState } from "react";
+import React from "react";
+import RangeInput from "./RangeInput";
+import { Link } from "react-router-dom";
 
 // this is the donate page with products and donation progress bar
 function DonatePage() {
-  const [value, setValue] = useState(50);
-
-  const handleInputChange = (event) => {
-    setValue(event.target.value);
-  };
-
-  const calculateBackground = () =>
-    `linear-gradient(to right, #009975 0%, #009975 ${value}%, gray ${value}%, gray 100%)`;
-
-  const tooltipPosition = {
-    left: `${(value / 100) * 100}%`,
-  };
-
   const projects = [
     {
       id: 1,
@@ -108,37 +97,10 @@ function DonatePage() {
                   A standalone content management platform for tech blogs and
                   news where everyone contributes.
                 </p>
-                <form className="my-2">
-                  <div className="relative">
-                    <input
-                      id="medium-range"
-                      type="range"
-                      value={value}
-                      min={0}
-                      max={100}
-                      onChange={handleInputChange}
-                      style={{
-                        background: calculateBackground(),
-                        height: "6px",
-                        borderRadius: "5px",
-                      }}
-                      className="w-full appearance-none cursor-pointer"
-                    />
-                    <label
-                      htmlFor="medium-range"
-                      className="absolute -top-3 transform -translate-x-1/2 text-xs font-medium text-[#009975]"
-                      style={tooltipPosition}
-                    >
-                      {value}%
-                    </label>
-                  </div>
-                  <div className="text-right text-sm text-[#0061CC]">
-                    KES 250,000
-                  </div>
-                </form>
+                <RangeInput />
                 <div className="flex items-center justify-between my-2">
                   <img src="/donate.png" alt="donate-img" />
-                  <div className="flex items-center gap-2">
+                  <Link to={`/donate/${project?.id}`} className="flex items-center gap-2">
                     <p className="text-[#009975]">View Project</p>
                     <svg
                       width="16"
@@ -152,7 +114,7 @@ function DonatePage() {
                         fill="#009975"
                       />
                     </svg>
-                  </div>
+                  </Link>
                 </div>
               </div>
             </div>
