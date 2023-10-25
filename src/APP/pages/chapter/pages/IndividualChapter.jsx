26d5774f5d0d9ed86b5d9416fb13906fff69f839
Collins Kasyoki <collinskasyoki@gmail.com>
2023-10-25 13:36:04 +0300

    Merge chapter endpoints

--- src/APP/pages/chapter/pages/IndividualChapter.jsx
+++ src/APP/pages/chapter/pages/IndividualChapter.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import WelcomeSection from '../sections/WelcomeSection';
-import AboutChapterSection from '../sections/AboutChapterSection';
-import UpcomingEventSection from '../sections/UpcomingEventSection';
-import ImpactSection from '../sections/ImpactSection';
-import OrganizersSection from '../sections/OrganizersSection';
-
+import { React, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import WelcomeSection from "../sections/WelcomeSection";
+import AboutChapterSection from "../sections/AboutChapterSection";
+import UpcomingEventSection from "../sections/UpcomingEventSection";
+import ImpactSection from "../sections/ImpactSection";
+import OrganizersSection from "../sections/OrganizersSection";
+import { fetchChapterData } from "./data";
 
 function IndividualChapter() {
-  // content-between md:px-20
+  const [chapterData, setChapterData] = useState(null);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const theData = await fetchChapterData(id);
+        setChapterData((prevState) => {
+          return prevState = theData;
+        });
+      } catch (error) {
+        console.error("Problem fetching chapter data:", error);
+      }
+    };
+    fetchData();
+  }, []);
 
   return (
-    <div>
-      <WelcomeSection />
-      <AboutChapterSection />
-      <UpcomingEventSection />
-      <ImpactSection />
-      <OrganizersSection />
-    </div>
-
-  )
+    <>
+      {chapterData ? (
+        <div>
+          <WelcomeSection chapter={chapterData} />
+          <AboutChapterSection chapter={chapterData} />
+          <UpcomingEventSection events={chapterData.events} />
+          <ImpactSection events_count={chapterData.events_count} />
+          <OrganizersSection organizers={chapterData.organizers} />
+        </div>
+      ) : (
+        <div>Loading chapter...</div>
+      )}
+    </>
+  );
 }
 
 export default IndividualChapter;
