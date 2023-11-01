62a5b344e1f2cfcd03deeff123687e20f8ba02ed
sonylomo <sonylomo1@gmail.com>
2023-11-01 10:31:09 +0300

    fix: added links

--- src/APP/pages/community/sections/WelcomeSection.jsx
+++ src/APP/pages/community/sections/WelcomeSection.jsx
@@ -28,9 +28,10 @@ function WelcomeSection() {
           </p>
 
           <div className="flex flex-col items-center justify-center md:flex-row">
-            <Link
-              type="button"
-              to="/"
+            <a
+              href="https://discord.com/invite/T6zTMWsnnS"
+              target="_blank"
+              rel="noopener noreferrer"
               className="text-white bg-[#7289DA] hover:bg-[#7289DA]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-3.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-6 mb-6"
             >
               <svg
@@ -44,11 +45,12 @@ function WelcomeSection() {
                 <path d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z" />
               </svg>
               Join our Discord
-            </Link>
+            </a>
 
-            <Link
-              type="button"
-              to="/"
+            <a
+              href="https://github.com/spaceyatech"
+              target="_blank"
+              rel="noopener noreferrer"
               className="text-white bg-[#171515] hover:bg-[#171515]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-3.5 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 mr-6 mb-6"
             >
               <svg
@@ -64,11 +66,12 @@ function WelcomeSection() {
                   clipRule="evenodd"
                 />
               </svg>
-              Join our Github
-            </Link>
-            <Link
-              type="button"
-              to="/"
+              Explore our Github
+            </a>
+            <a
+              href="https://x.com/SpaceYaTech"
+              target="_blank"
+              rel="noopener noreferrer"
               className="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-5 py-3.5 text-center inline-flex items-center dark:focus:ring-[#1da1f2]/55 mr-6 mb-6"
             >
               <svg
@@ -85,7 +88,7 @@ function WelcomeSection() {
                 />
               </svg>
               Follow our Twitter
-            </Link>
+            </a>
           </div>
         </div>
       </div>
