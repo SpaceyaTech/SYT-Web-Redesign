c837cb0878fb8eac784edde805222c1876d8f1c2
felixNyalenda <felixasejunior@gmail.com>
2023-11-01 16:18:19 +0300

    -

--- src/APP/components/Footer2.jsx
+++ src/APP/components/Footer2.jsx
@@ -1,4 +1,5 @@
-import React, { Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import logo from "../../assets/images/sytLogo.png";
 import backup from "../../assets/images/backup.svg";
