62885db64eac28da2fa25cae13841413e0fe6e41
Collins Kasyoki <collinskasyoki@gmail.com>
2023-11-03 10:42:02 +0300

    Switch top events data fetching to react-query

--- src/APP/pages/events/sections/eventsSection/EventsSection.jsx
+++ src/APP/pages/events/sections/eventsSection/EventsSection.jsx
@@ -2,45 +2,33 @@ import React, { useEffect, useState } from 'react';
 import EventsUpdateSection from './EventsUpdateSection';
 import EventsTab from './EventsTab'
 import Events from './Events';
-import {
-  community,
-  MasterBase,
-  mpesapayments,
-  techrecruiters,
-  mentorlst,
-  uxhiringafrica,
-} from '../../../../../assets/images/community';
+// import {
+//   community,
+//   MasterBase,
+//   mpesapayments,
+//   techrecruiters,
+//   mentorlst,
+//   uxhiringafrica,
+// } from '../../../../../assets/images/community';
 import { fetchEventData } from '../../../community/sections/eventsSection/data';
+import { useQuery } from 'react-query';
 
 function EventsSection({showTabs, showAllEventsLink}) {
-  const [topEventsData, setTopEventsData] = useState(null);
   const [filters, setFilters] = useState({});
+  const { data: topEventsData, status } = useQuery("topevents", () => fetchEventData(filters));
 
-  useEffect (()=> {
-    const fetchData = async () => {
-      try {
-        const theData = await fetchEventData(filters);
-        setTopEventsData((prevState) => {
-          return prevState = theData;
-        });
-      } catch(error) {
-        // Handle error
-        console.error("Error fetching top events: ", error);
-      }
-    }
-    fetchData();
-  }, []);
-  
   return (
-    <div className="p-6">
+    <>
+    {status === "error" && <p>Error!</p>}
+    {status === "loading" && <p>Loading...</p>}
+    {status === "success" && (
+      <div className="p-6">
       <EventsUpdateSection showAllEventsLink={showAllEventsLink} />
       {showTabs && <EventsTab />}
-      {topEventsData ? (
-        <Events events={topEventsData.results} isVertical={false} />
-      ) : (
-        <div>Loading Recent Events...</div>
-      )}
+      <Events events={topEventsData.results} isVertical={false} />
     </div>
+    )};
+    </>
   );
 }
 
