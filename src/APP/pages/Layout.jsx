9039082945c290c8d990b1888c93f86faa550f43
Collins Kasyoki <collinskasyoki@gmail.com>
2023-11-03 08:59:25 +0300

    Add scroll to top effect on click of new pages

--- src/APP/pages/Layout.jsx
+++ src/APP/pages/Layout.jsx
@@ -1,11 +1,23 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { Header2, Footer2 } from "../components";
+import { useEffect } from "react";
+
+const ScrollToTopOnLinkClick = () => {
+  const{ pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 const Layout = () => {
   return (
     <div className="w-screen overflow-hidden">
       <Header2 />
+      <ScrollToTopOnLinkClick />
       <Outlet />
       <Footer2 />
     </div>
