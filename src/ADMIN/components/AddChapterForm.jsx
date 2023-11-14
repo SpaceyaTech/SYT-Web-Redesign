c867b42470ef135eec3678dad1b1115b835b3518
Collins Kasyoki <collinskasyoki@gmail.com>
2023-11-14 16:02:18 +0300

    Integrate add chapter functionality

--- src/ADMIN/components/AddChapterForm.jsx
+++ src/ADMIN/components/AddChapterForm.jsx
@@ -1,22 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
 import GeneralInfo from "./GeneralInfo";
 import SocialMediaLinks from "./SocialMediaLinks";
 import Organizers from "./Organizers";
 
-function AddChapterForm({ currentStep }) {
+function AddChapterForm({ currentStep, theNext, thePrevious, collectedChapter, setCollectedChapter, formComplete }) {
   switch (currentStep) {
     case 0:
-      return <GeneralInfo />;
+      return <GeneralInfo theNext={theNext} setCollectedChapter={setCollectedChapter} />;
     case 1:
-      return <SocialMediaLinks />;
+      return <SocialMediaLinks thePrevious={thePrevious} theNext={theNext} setCollectedChapter={setCollectedChapter} />;
     case 2:
-      return <Organizers />;
+      return <Organizers thePrevious={thePrevious} collectedChapter={collectedChapter} formComplete={formComplete} />;
     default:
       return null;
   }
 }
 AddChapterForm.propTypes = {
-  currentStep: null,
+  currentStep: PropTypes.number.isRequired,
 };
 
 AddChapterForm.defaultProps = {
