c52d0d63a7f18f0f0cfa2bfcb6177cf7098f39a7
Sonia Lomo <49971500+sonylomo@users.noreply.github.com>
2023-11-16 13:17:07 +0300

    fix: correct image url

--- src/assets/images/aboutPage/index.js
+++ src/assets/images/aboutPage/index.js
@@ -1,16 +1,16 @@
-import aaron from "./creativeTeam/aaron-abubakar.png";
-import emmy from "./creativeTeam/emmy-akinyi.png";
+import aaron from "./creativeTeam/aaron-abubakar.jpeg";
+import emmy from "./creativeTeam/emmy-akinyi.jpeg";
 import pamela from "./creativeTeam/pamela-owino.png";
 
-import catherine from "./leadership/catherine-kiiru.png";
-import fred from "./leadership/fred-ouko.png";
+import catherine from "./leadership/catherine-kiiru.jpeg";
+import fred from "./leadership/fred-ouko.jpeg";
 import hudson from "./leadership/hudson-obai.png";
 import ian from "./leadership/ian-mugenya.png";
-import james from "./leadership/james-otieno.png";
+import james from "./leadership/james-otieno.jpeg";
 import juma from "./leadership/juma-lawrence.png";
 import marlyn from "./leadership/marlyn-mayienga.png";
-import sharon from "./leadership/sharon-jebitok.png";
-import waithaka from "./leadership/waithaka-waweru.png";
+import sharon from "./leadership/sharon-jebitok.jpeg";
+import waithaka from "./leadership/waithaka-waweru.jpeg";
 
 import about1 from "./about-img-1.png";
 import about2 from "./about-img-2.png";
