afffc49f6bbd5151016406f81a92a52b2ad750a5
felixNyalenda <felixasejunior@gmail.com>
2023-11-16 09:12:35 +0300

    changed hero images on aboutUs & landing pages

--- src/APP/components/PodcastCard.jsx
+++ src/APP/components/PodcastCard.jsx
@@ -5,7 +5,7 @@ function PodcastCard({ img, category = "Podcast", title, description, link }) {
     <a href={link} target="_blank" rel="noopener noreferrer">
       <div className="w-[95vw] md:w-[520px] h-[520px] rounded-md overflow-hidden relative">
         <img src={img} alt="podcast" className="w-full h-full object-cover" />
-        <div className="absolute bottom-0 left-0 right-0 bg-black opacity-80 h-1/3" />
+        <div className="absolute bottom-0 left-0 right-0 bg-black opacity-80 h-1/2 md:h-1/3" />
 
         <div className="absolute bottom-0 left-0 right-0 text-white flex flex-col gap-3 p-6">
           <h5 className="text-[#009975] font-medium text-sm capitalize">
---
+++ src/assets/images/aboutPage/index.js
@@ -0,0 +1,4 @@
+import heroImg from "./heroSec.png";
+import aboutImg2 from "./about-img-2.png";
+
+export { heroImg, aboutImg2 };
