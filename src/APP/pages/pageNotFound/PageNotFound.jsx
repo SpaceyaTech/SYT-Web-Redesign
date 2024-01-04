1482eac1dab57cb4b35e4640e9cbe5c7190a6105
felixNyalenda <felixasejunior@gmail.com>
2024-01-04 16:46:57 +0300

    import 404 image

--- src/APP/pages/pageNotFound/PageNotFound.jsx
+++ src/APP/pages/pageNotFound/PageNotFound.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-const PageNotFound = () => {
+import pageNotFound from "../../../assets/images/pageNotFound.svg";
+
+function PageNotFound() {
   return (
-    <section className="max-w-[1440px] mx-auto border">PageNotFound</section>
+    <section className="max-w-[1440px] mx-auto border flex flex-col items-center gap-8">
+      <img
+        src={pageNotFound}
+        alt="pageNotFound"
+        className="h-full p-4 sm:h-[400px] object-cover w-full sm:w-fit border"
+      />
+      <div className="">ase</div>
+    </section>
   );
-};
+}
 
 export default PageNotFound;
