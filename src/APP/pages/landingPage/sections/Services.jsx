55603c49a5dd2e2aa62d6b32f28bda9509dccb9a
felixNyalenda <felixasejunior@gmail.com>
2024-01-06 23:42:52 +0300

    span components on the landing page

--- src/APP/pages/landingPage/sections/Services.jsx
+++ src/APP/pages/landingPage/sections/Services.jsx
@@ -45,29 +45,31 @@ function SytServices({ service }) {
 
 function Services() {
   return (
-    <section className="text-gray-600 body-font flex items-center bg-[#F7F7F7]">
-      <div className="hidden md:flex w-full md:w-1/2 justify-center items-center py-20">
+    <section className="text-gray-600 body-font flex items-center bg-[#F7F7F7] mx-auto justify-center">
+      <div className="flex-half w-1/2 hidden md:flex justify-end">
         <img
-          className="object-cover object-center rounded w-[569px] h-[650px]"
+          className="object-cover object-center rounded"
           alt="services"
           src={serviceImg}
         />
       </div>
-      <div className="flex flex-col flex-wrap bg-[#00664E] lg:py-6 md:w-1/2 lg:pl-12 lg:text-left text-center">
-        <div className="flex flex-col gap-y-4 mt-4 mb-4 md:mb-10">
-          <h2 className="text-sm text-[#BDF16D] tracking-widest font-medium title-font mb-1">
-            How we work
-          </h2>
-          <h1 className="sm:text-3xl text-2xl tracking-wide font-medium title-font text-white">
-            Getting into tech can be hard, but it doesn’t have to be
-          </h1>
-        </div>
 
-        {services.map((service) => (
-          <SytServices key={service.id} service={service} />
-        ))}
+      <div className="w-full flex justify-start flex-col flex-wrap bg-[#00664E] lg:py-6 md:flex-half lg:pl-12 lg:text-left text-center">
+        <div className="max-w-[720px]">
+          <div className="flex flex-col gap-y-4 mt-4 mb-4 md:mb-10">
+            <h2 className="text-sm text-[#BDF16D] tracking-widest font-medium title-font mb-1">
+              How we work
+            </h2>
+            <h1 className="sm:text-3xl text-2xl tracking-wide font-medium title-font text-white">
+              Getting into tech can be hard, but it doesn’t have to be
+            </h1>
+          </div>
+
+          {services.map((service) => (
+            <SytServices key={service.id} service={service} />
+          ))}
+        </div>
       </div>
-      {/* </div> */}
     </section>
   );
 }
