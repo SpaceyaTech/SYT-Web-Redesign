21381d4d31fdc0fe268c69015f0a574c25beda5b
felixNyalenda <felixasejunior@gmail.com>
2024-01-06 11:12:16 +0300

    refactor service section

---
+++ src/assets/images/services-section/index.js
@@ -0,0 +1,6 @@
+import serviceImg from "./services_image.png";
+import twitter from "./twitter-space.svg";
+import openSource from "./open-source.svg";
+import mentorship from "./mentorship.svg";
+
+export { serviceImg, twitter, openSource, mentorship };
