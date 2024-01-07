8b414d1eddb6a69328aeb09b0345574e576bc4cb
felixNyalenda <felixasejunior@gmail.com>
2024-01-07 20:27:44 +0300

    add error img

--- src/APP/pages/pageNotFound/PageNotFound.jsx
+++ src/APP/pages/pageNotFound/PageNotFound.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-import pageNotFound from "../../../assets/images/pageNotFound.svg";
+import { error404 } from "../../../assets/images/errorPage";
 
 function PageNotFound() {
   const navigate = useNavigate();
@@ -9,9 +9,9 @@ function PageNotFound() {
   return (
     <section className="max-w-[1440px] mx-auto flex flex-col items-center gap-0 pb-10 md:pb-8">
       <img
-        src={pageNotFound}
+        src={error404}
         alt="pageNotFound"
-        className="px-4 sm:px-12 my-8 sm:my-0 h-[400px] md:h-[540px] object-contain w-full sm:w-fit"
+        className="p-4 mt-5 md:my-10 sm:px-12 sm:my-0 h-[400px] md:h-[540px] object-contain w-full sm:w-fit"
       />
 
       <div className="flex flex-col items-center justify-center gap-2 sm:gap-3 text-center max-w-lg">
---
+++ src/assets/images/errorPage/index.js
@@ -0,0 +1,6 @@
+import error400 from "./error-400.png";
+import error403 from "./error-403.png";
+import error404 from "./error-404.svg";
+import error500 from "./error-500.png";
+
+export { error400, error403, error404, error500 };
