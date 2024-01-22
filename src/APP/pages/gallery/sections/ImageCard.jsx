b222ab876bf321e3fd49935c95dba49ea352535a
felixNyalenda <felixasejunior@gmail.com>
2024-01-22 15:58:30 +0300

    add hover effect

--- src/APP/pages/gallery/sections/ImageCard.jsx
+++ src/APP/pages/gallery/sections/ImageCard.jsx
@@ -22,11 +22,23 @@ function ImageCard({ photos }) {
       <PhotoAlbum
         photos={photos}
         layout="masonry"
-        // targetRowHeight={450}
+        spacing={0}
         onClick={({ index }) => setIndex(index)}
-        renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto }) => (
-          <div style={wrapperStyle}>
+        // targetRowHeight={450}
+        renderPhoto={({ wrapperStyle, renderDefaultPhoto }) => (
+          <div style={wrapperStyle} className="relative">
             {renderDefaultPhoto({ wrapped: true })}
+
+            <div className="absolute top-0 right-0 w-full h-full flex opacity-0 transition-all duration-500 ease-linear hover:opacity-100">
+              <div className="bg-transparent w-1/2 h-full" />
+              <div className="bg-[#00664E] text-white w-1/2 h-full flex items-center justify-center flex-col gap-2">
+                <span className="text-xs font-normal">Jan 12, 2023</span>
+
+                <div className="w-1/5 h-[2px] bg-white rounded-sm" />
+
+                <h4 className="text-lg font-medium text-center">SYT Meetup</h4>
+              </div>
+            </div>
           </div>
         )}
       />
@@ -37,12 +49,7 @@ function ImageCard({ photos }) {
         index={index}
         close={() => setIndex(-1)}
         // enable optional lightbox plugins
-        plugins={[
-          // Fullscreen,
-          //   Slideshow,
-          Thumbnails,
-          Zoom,
-        ]}
+        plugins={[Thumbnails, Zoom]}
       />
     </div>
   );
