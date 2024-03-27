f45302ed131398acf66afa52759d4334af4db43d
bkm-code <bridgit.k.mutinda@gmail.com>
2024-03-27 11:59:48 +0300

    Imported ellipse styles

--- src/APP/pages/landingPage/sections/HeroSection3.jsx
+++ src/APP/pages/landingPage/sections/HeroSection3.jsx
@@ -4,21 +4,23 @@ import Marquee from "react-fast-marquee";
 
 import { partners } from "../data";
 import { bannerImg } from "../../../../assets/images/hero-section";
+import "./../../../../index.css";
 
 function HeroSection3() {
   return (
-    <section className="p-3 md:px-10 flex flex-col gap-2 max-w-[1440px] mx-auto">
+    <section className="p-3 md:px-10 flex flex-col items-center justify-center gap-2 max-w-[1440px] mx-auto">
       {/* Hero Header */}
-      <div className="flex flex-col-reverse md:flex-row items-center gap-8 md:gap-4">
+      <div className="flex flex-col items-center justify-center gap-8 md:gap-4 max-w-[670px]">
         {/* hero desc */}
-        <div className="flex-1 flex flex-col gap-6">
-          <h1 className="md:text-[40px] text-2xl leading-8 md:leading-normal font-medium text-gray-900 text-center md:text-left">
-            Accelerate your <span className="text-primary">growth</span> and
-            unlock your potential in the tech ecosphere
+        <div className="flex-1 flex flex-col items-center justify-center gap-6 text-center md:text-center">
+          <h1 className="md:text-[40px] text-2xl leading-8 md:leading-normal font-medium text-gray-900">
+            Accelerate your <span className="text-primary">growth</span> and{" "}
+            <span className="text-primary">potential</span> of the tech
+            enthusiasts
           </h1>
-          <p className="text-sm md:text-xl font-normal leading-relaxed text-center md:text-left">
-            The fastest growing Africa, open-source community looking to change
-            the way young Africans get started in technology.
+          <p className="text-sm md:text-xl font-normal leading-relaxed">
+            SpaceYaTech is the fastest growing Africa, open-source community
+            looking to change the way young Africans get started in technology.
           </p>
 
           <Link
@@ -31,12 +33,100 @@ function HeroSection3() {
         </div>
 
         {/* hero img */}
-        <div className="flex-1">
-          <img
-            src={bannerImg}
-            alt="banner"
-            className="w-full h-full object-cover"
-          />
+
+        <div className="flex flex-col items-center border border-yellow-500 min-w-[1440px] min-h-[700px] rounded-[70%] relative">
+          <div className="relative creative-shapes">
+            <div className="ellipse115 rounded-full" />
+            <div className="ellipse116 rounded-full" />
+            <div className="ellipse117 rounded-full" />
+            <div className="ellipse118 rounded-full" />
+            <div className="ellipse132 rounded-full" />
+            <div className="ellipse119 rounded-full" />
+            <div className="ellipse133 rounded-full" />
+            <div className="ellipse134 rounded-full" />
+            <div className="ellipse172 rounded-full" />
+            <div className="ellipse173 rounded-full" />
+            <div className="ellipse174 rounded-full" />
+            <div className="ellipse175 rounded-full" />
+            <div className="ellipse176 rounded-full" />
+            <div className="ellipse135 rounded-full" />
+            <div className="ellipse136 rounded-full" />
+            <div className="ellipse120 rounded-full" />
+            <div className="ellipse124 rounded-full" />
+            <div className="ellipse125 rounded-full" />
+            <div className="ellipse126 rounded-full" />
+            <div className="ellipse121 rounded-full" />
+            <div className="ellipse127 rounded-full" />
+            <div className="ellipse128 rounded-full" />
+            <div className="ellipse129 rounded-full" />
+            <div className="ellipse130 rounded-full" />
+            <div className="ellipse131 rounded-full" />
+            <div className="ellipse122 rounded-full" />
+            <div className="ellipse123 rounded-full" />
+            <div className="ellipse137 rounded-full" />
+            <div className="ellipse138 rounded-full" />
+            <div className="ellipse139 rounded-full" />
+            <div className="ellipse140 rounded-full" />
+            <div className="ellipse141 rounded-full" />
+            <div className="ellipse142 rounded-full" />
+            <div className="ellipse143 rounded-full" />
+            <div className="ellipse144 rounded-full" />
+            <div className="ellipse145 rounded-full" />
+            <div className="ellipse177 rounded-full" />
+            <div className="ellipse178 rounded-full" />
+            <div className="ellipse146 rounded-full" />
+            <div className="ellipse156 rounded-full" />
+            <div className="ellipse157 rounded-full" />
+            <div className="ellipse179 rounded-full" />
+            <div className="ellipse158 rounded-full" />
+            <div className="ellipse164 rounded-full" />
+            <div className="ellipse165 rounded-full" />
+            <div className="ellipse167 rounded-full" />
+            <div className="ellipse168 rounded-full" />
+            <div className="ellipse169 rounded-full" />
+            <div className="ellipse170 rounded-full" />
+            <div className="ellipse171 rounded-full" />
+            <div className="ellipse166 rounded-full" />
+            <div className="ellipse147 rounded-full" />
+            <div className="ellipse148 rounded-full" />
+            <div className="ellipse151 rounded-full" />
+            <div className="ellipse149 rounded-full" />
+            <div className="ellipse152 rounded-full" />
+            <div className="ellipse153 rounded-full" />
+            <div className="ellipse159 rounded-full" />
+            <div className="ellipse160 rounded-full" />
+            <div className="ellipse161 rounded-full" />
+            <div className="ellipse162 rounded-full" />
+            <div className="ellipse163 rounded-full" />
+            <div className="ellipse154 rounded-full" />
+            <div className="ellipse155 rounded-full" />
+            <div className="ellipse150 rounded-full" />
+            <div className="ellipse147 rounded-full" />
+            <div className="ellipse1411 rounded-full" />
+            <div className="ellipse128 rounded-full" />
+            <div className="ellipse142 rounded-full" />
+            <div className="ellipse175 rounded-full" />
+            <div className="ellipse146 rounded-full" />
+            <div className="ellipse159 rounded-full" />
+            <div className="ellipse167 rounded-full" />
+            <div className="ellipse160 rounded-full" />
+            <div className="ellipse118 rounded-full" />
+            <div className="ellipse138 rounded-full" />
+            <div className="ellipse141 rounded-full" />
+            <div className="ellipse157 rounded-full" />
+            <div className="ellipse132 rounded-full" />
+            <div className="ellipse131 rounded-full" />
+            <div className="ellipse136 rounded-full" />
+            <div className="ellipse124 rounded-full" />
+            <div className="ellipse164 rounded-full" />
+            <div className="ellipse165 rounded-full" />
+            <div className="ellipse148 rounded-full" />
+            <div className="ellipse152 rounded-full" />
+            <div className="ellipse153 rounded-full" />
+            <div className="ellipse155 rounded-full" />
+            <div className="ellipse127 rounded-full" />
+            <div className="ellipse117 rounded-full" />
+          </div>
         </div>
       </div>
 
