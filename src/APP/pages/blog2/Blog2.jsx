fb8cb4359678ed0ee1c201217431814e9563dc42
alvyynm <alvyynm@gmail.com>
2024-05-31 22:27:36 +0300

    feat: add seo metadata to/blogs:titleSlug

--- src/APP/pages/blog2/Blog2.jsx
+++ src/APP/pages/blog2/Blog2.jsx
@@ -3,6 +3,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import useBlogData from "../../../hooks/Queries/blog/useBlogData";
 import { Loader } from "../../components";
+import SeoMetadata from "../../components/SeoMetadata";
 import { Advert, BlogHeader, BlogBody } from "./sections";
 
 function Blog2() {
@@ -22,38 +23,49 @@ function Blog2() {
   }, [refetchBlogData, titleSlug]);
 
   return (
-    <div className="max-w-[1024px] mx-auto">
-      {isError && navigate("/error-500")}
+    <>
+      <SeoMetadata
+        title={blogData?.title}
+        description={blogData?.description}
+        type="article"
+        url={blogData?.title_slug}
+        ogImage={blogData?.image}
+        ogImageAlt="SpaceYaTech logo, social media handles, website URL, email, and more on a muted background."
+        siteName="SpaceYaTech Blog"
+      />
+      <div className="max-w-[1024px] mx-auto">
+        {isError && navigate("/error-500")}
 
-      {isLoading && (
-        <div className="flex flex-col items-center justify-center gap-4 py-10">
-          <Loader />
-          <p className="text-lg font-medium text-primary">
-            Loading blog details...
-          </p>
-        </div>
-      )}
-      {isSuccess && (
-        <>
-          <Advert />
-          <BlogHeader
-            title={blogData?.title}
-            description={blogData?.description}
-            image={blogData?.image}
-            author={blogData?.author}
-            createdAt={blogData?.created_at}
-            id={blogData?.id}
-            likes={blogData?.likes}
-            titleSlug={blogData?.title_slug}
-          />
-          <BlogBody
-            id={blogData?.id}
-            categoryId={blogData?.category.id}
-            blogBody={blogData?.body}
-          />
-        </>
-      )}
-    </div>
+        {isLoading && (
+          <div className="flex flex-col items-center justify-center gap-4 py-10">
+            <Loader />
+            <p className="text-lg font-medium text-primary">
+              Loading blog details...
+            </p>
+          </div>
+        )}
+        {isSuccess && (
+          <>
+            <Advert />
+            <BlogHeader
+              title={blogData?.title}
+              description={blogData?.description}
+              image={blogData?.image}
+              author={blogData?.author}
+              createdAt={blogData?.created_at}
+              id={blogData?.id}
+              likes={blogData?.likes}
+              titleSlug={blogData?.title_slug}
+            />
+            <BlogBody
+              id={blogData?.id}
+              categoryId={blogData?.category.id}
+              blogBody={blogData?.body}
+            />
+          </>
+        )}
+      </div>
+    </>
   );
 }
 
