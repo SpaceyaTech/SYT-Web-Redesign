dec0f5c73361c749d4e3e9b6bdd55a3aaae13277
eric githaiga <ericgithaiga007@gmail.com>
2024-06-05 11:10:35 +0300

    doc: updated delete unused assets issue

--- src/assets/images/blogs-page/index.js
+++ src/assets/images/blogs-page/index.js
@@ -1,29 +1,12 @@
 import arrowRight from "./arrow-right.png";
-import blog1 from "./blog1.png";
-import chatText from "./chatText.svg";
-import Ellipse30 from "./Ellipse30.png";
-import eye from "./eye.svg";
-import featured from "./featured.png";
+
 import advert from "./Frame.png";
 import glovo from "./glovo.png";
-import iphone from "./iphone.png";
-import plasma from "./plasma.jpg";
-import reply from "./reply.svg";
+
 import search from "./search.png";
 import thumbsUp from "./thumbsUp.svg";
 
 export {
-  search,
-  blog1,
-  advert,
-  glovo,
-  iphone,
-  arrowRight,
-  chatText,
-  Ellipse30,
-  thumbsUp,
-  eye,
-  reply,
-  featured,
-  plasma,
-};
+   search, advert, glovo, arrowRight, thumbsUp
+   };
+
