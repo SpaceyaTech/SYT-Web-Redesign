52cbc7185c64c89e0f17763d83a0ad2a18090e34
felixNyalenda <felixasejunior@gmail.com>
2024-06-06 11:31:06 +0300

    extracted the FallbackLoader to its own component

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from "react";
+import React, { Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
 
 // Lazy FallbackLoader components
