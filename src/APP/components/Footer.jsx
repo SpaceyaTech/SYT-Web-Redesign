5066fac84792ec7b0dc01a3bb2f9ec3a13f4ecf7
felixNyalenda <felixasejunior@gmail.com>
2024-06-11 08:58:36 +0300

    fix footer subscribe form width overflow

--- src/APP/components/Footer.jsx
+++ src/APP/components/Footer.jsx
@@ -1,81 +1,18 @@
+/* eslint-disable function-paren-newline */
+/* eslint-disable import/no-cycle */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable implicit-arrow-linebreak */
 /* eslint-disable no-confusing-arrow */
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { Link } from "react-router-dom";
-import {
-  linkedin,
-  twitter,
-  spotify,
-  youtube,
-  instagram,
-  facebook,
-} from "../../assets/images/socials";
+
 import logo from "../../assets/images/sytLogo.png";
+import { footerLinks, socialLinks } from ".";
 
 function Footer() {
   const now = new Date();
   const year = now.getFullYear();
-
-  const socialLinks = [
-    {
-      href: "https://www.facebook.com/spaceyatech",
-      src: facebook,
-      alt: "facebook",
-    },
-    {
-      href: "https://www.instagram.com/spaceyatech/",
-      src: instagram,
-      alt: "instagram",
-    },
-    { href: "https://x.com/SpaceYaTech", src: twitter, alt: "twitter" },
-    {
-      href: "https://linkedin.com/company/spaceyatech",
-      src: linkedin,
-      alt: "linkedIn",
-    },
-    {
-      href: "https://www.youtube.com/@spaceyatech",
-      src: youtube,
-      alt: "youtube",
-    },
-    {
-      href: "https://open.spotify.com/show/4nUYzhacDAw1v9ClqPY89n",
-      src: spotify,
-      alt: "spotify",
-    },
-  ];
-
-  const footerLinks = [
-    {
-      title: "Company",
-      links: [
-        { to: "/about-us", name: "About Us" },
-        { to: "/community", name: "Community" },
-        { to: "/products", name: "Products" },
-        { to: "/shop", name: "Shop" },
-      ],
-    },
-    {
-      title: "Resources",
-      links: [
-        { href: "/events", name: "Events" },
-        { href: "http://mentorlst.com", name: "Mentorship" },
-        { href: "/gallery", name: "Gallery" },
-      ],
-    },
-    {
-      title: "Quick Links",
-      links: [
-        { href: "https://x.com/SpaceYaTech", name: "Contact Us" },
-        {
-          href: "https://syt-terms.notion.site/SpaceYaTech-Terms-of-Service-7d84de7a4feb41cc9f86143a9cc572e0",
-          name: "Terms and Conditions",
-        },
-      ],
-    },
-  ];
 
   return (
     <footer className="bg-secondary text-white">
@@ -125,61 +62,65 @@ function Footer() {
           </div>
 
           {/* Footer links */}
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-12 sm:gap-8">
+            {footerLinks.map((section) => (
+              <div
+                key={crypto.randomUUID()}
+                className="flex flex-col gap-8 text-center lg:text-left"
+              >
+                <h2 className="text-green-footer font-bold sm:text-xl text-lg leading-9">
+                  {section.title}
+                </h2>
 
-          {footerLinks.map((section) => (
-            <div key={crypto.randomUUID()} className="flex flex-col gap-8">
-              <h2 className="text-[#00FFC3] font-bold sm:text-xl text-lg leading-9">
-                {section.title}
-              </h2>
-
-              <ul className="flex flex-col gap-5 list-none text-sm font-light">
-                {section.links.map((link) =>
-                  link.href ? (
-                    <li key={crypto.randomUUID()}>
-                      <a
-                        href={link.href}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className="hover:underline text-white"
-                      >
-                        {link.name}
-                      </a>
-                    </li>
-                  ) : (
-                    <li key={crypto.randomUUID()}>
-                      <Link to={link.to}>{link.name}</Link>
-                    </li>
-                  )
-                )}
-              </ul>
-            </div>
-          ))}
+                <ul className="flex flex-col gap-5 list-none text-sm font-light">
+                  {section.links.map((link) =>
+                    link.href ? (
+                      <li key={crypto.randomUUID()}>
+                        <a
+                          href={link.href}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                          className="hover:underline text-white"
+                        >
+                          {link.name}
+                        </a>
+                      </li>
+                    ) : (
+                      <li key={crypto.randomUUID()}>
+                        <Link to={link.to}>{link.name}</Link>
+                      </li>
+                    )
+                  )}
+                </ul>
+              </div>
+            ))}
+          </div>
 
           {/* Newsletter */}
           <div className="flex-2">
-            <div className="flex flex-col gap-8">
-              <h2 className="text-[#00FFC3] font-bold sm:text-xl text-lg leading-9">
+            <div className="flex flex-col md:gap-8">
+              <h2 className="text-green-footer font-bold sm:text-xl text-lg md:leading-9 mx-auto lg:mx-0 text-center lg:text-left">
                 Subscribe to our Newsletter
               </h2>
 
               <div className="flex flex-col gap-5 text-sm font-light">
-                <p className="text-sm left-6">
+                <p className="text-sm left-6 text-center lg:text-left">
                   Subscribe to our newsletter to get your
-                  <br />
+                  <br className="hidden sm:block" />
                   weekly dose of news, updates, tips and even
-                  <br />
+                  <br className="hidden sm:block" />
                   special offers
                 </p>
 
-                <form className="flex items-center">
+                <form className="flex items-center max-w-md w-full mx-auto">
                   <input
                     type="email"
                     placeholder="Enter email address"
-                    className="w-auto px-2 md:px-4 py-3 rounded-lg rounded-r-none flex-3 outline-none text-black font-medium"
+                    className="w-2/3 sm:w-auto px-2 md:px-4 py-3 rounded-lg rounded-r-none flex-3 outline-none text-black font-medium"
                   />
                   <button
                     type="submit"
-                    className="rounded-r-lg px-2 md:px-4 py-3 flex-auto font-semibold text-white transition-all duration-200 bg-primary border border-transparent hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
+                    className="rounded-r-lg px-2 md:px-4 py-3 flex-auto font-semibold text-white transition-all duration-200 bg-primary border-none hover:bg-gray-700 focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                   >
                     Subscribe
                   </button>
@@ -190,16 +131,10 @@ function Footer() {
         </div>
 
         <div className="sm:mt-14 mt-10">
-          <h1 className="text-center text-[#00FFC3] my-4 md:my-8">
+          <h1 className="text-center text-green-footer my-4 md:my-8">
             &copy; {year} SpaceYaTech | All Rights Reserved
           </h1>
         </div>
-        {/* <LazyLoadImage
-          src={backup}
-          alt="backup"
-          className="w-7 h-7"
-
-        /> */}
       </div>
     </footer>
   );
