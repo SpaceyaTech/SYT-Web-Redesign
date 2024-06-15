913ec25831dca7ad749c14c19d1c768dc38b22eb
Nyandika Donald <donald.nyandika@gmail.com>
2024-06-15 17:05:01 +0100

    Add unit and e2e tests

--- src/APP/pages/landingPage/sections/HeroSection.jsx
+++ src/APP/pages/landingPage/sections/HeroSection.jsx
@@ -52,6 +52,7 @@ function HeroSection() {
         </p>
 
         <button
+          id="hero-join-button"
           type="button"
           onClick={openModal}
           className="text-white bg-gradient-to-b to-primary from-green-dark border-0 py-3 px-4 md:px-8 focus:outline-none rounded-lg text-sm md:text-base w-fit text-center"
