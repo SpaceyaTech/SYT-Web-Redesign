778868cd36c9b1a5f862d941e8d382890c8f7b5d
alvyynm <alvyynm@gmail.com>
2024-06-21 12:08:03 +0300

    chore: move the returned value immediately after the `=>`

--- src/APP/pages/shop/sections/PopularItemsSection.jsx
+++ src/APP/pages/shop/sections/PopularItemsSection.jsx
@@ -30,56 +30,54 @@ function PopularItemsSection() {
       <div className="mt-8 grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:gap-x-16">
         {products
           ?.slice(0, 8)
-          .map(({ stock, category, description, id, name, image, price }) => {
-            return (
-              <div
-                key={id}
-                className="group relative border border-slate-200 p-2 rounded-md"
-                onClick={() => navigate(`/shop/item/${id}`)}
-              >
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
-                  <LazyLoadImage
-                    src={image}
-                    alt={name}
-                    className="w-full h-60 object-cover object-center lg:h-full lg:w-full"
-                  />
-                </div>
-                <div className="mt-4">
-                  <div className="space-y-2">
-                    <div className="flex justify-between">
-                      <h4 className="text-base text-gray-700 uppercase">
-                        <span
-                          aria-hidden="true"
-                          className="absolute inset-0"
-                        ></span>
-                        {name}
-                      </h4>
-                      <div>
-                        {stock ? (
-                          <p className="font-medium bg-[#ECFDF3] text-[#027A48] text-sm rounded-full px-2 py-1">
-                            {stock} items left
-                          </p>
-                        ) : (
-                          <p className="font-medium bg-[#FEF3F2] text-[#B42318] text-sm rounded-full px-2 py-1">
-                            Out of stock
-                          </p>
-                        )}
-                      </div>
+          .map(({ stock, category, description, id, name, image, price }) => (
+            <div
+              key={id}
+              className="group relative border border-slate-200 p-2 rounded-md"
+              onClick={() => navigate(`/shop/item/${id}`)}
+            >
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                <LazyLoadImage
+                  src={image}
+                  alt={name}
+                  className="w-full h-60 object-cover object-center lg:h-full lg:w-full"
+                />
+              </div>
+              <div className="mt-4">
+                <div className="space-y-2">
+                  <div className="flex justify-between">
+                    <h4 className="text-base text-gray-700 uppercase">
+                      <span
+                        aria-hidden="true"
+                        className="absolute inset-0"
+                      ></span>
+                      {name}
+                    </h4>
+                    <div>
+                      {stock ? (
+                        <p className="font-medium bg-[#ECFDF3] text-[#027A48] text-sm rounded-full px-2 py-1">
+                          {stock} items left
+                        </p>
+                      ) : (
+                        <p className="font-medium bg-[#FEF3F2] text-[#B42318] text-sm rounded-full px-2 py-1">
+                          Out of stock
+                        </p>
+                      )}
                     </div>
-                    <div className="flex justify-between">
-                      <p className="flex justify-between items-center gap-1 font-medium bg-[#FEF3F2] text-[#B42318] text-sm rounded-full px-2 py-1">
-                        <CiShoppingTag />
-                        {category}
-                      </p>
-                    </div>
-                    <p className="text-xl font-bold text-gray-500">
-                      KES {formatPrice(price)}
+                  </div>
+                  <div className="flex justify-between">
+                    <p className="flex justify-between items-center gap-1 font-medium bg-[#FEF3F2] text-[#B42318] text-sm rounded-full px-2 py-1">
+                      <CiShoppingTag />
+                      {category}
                     </p>
                   </div>
+                  <p className="text-xl font-bold text-gray-500">
+                    KES {formatPrice(price)}
+                  </p>
                 </div>
               </div>
-            );
-          })}
+            </div>
+          ))}
       </div>
     </div>
   );
