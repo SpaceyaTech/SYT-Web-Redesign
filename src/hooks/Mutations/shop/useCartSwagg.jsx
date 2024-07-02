73821c5f19e42cdd6a61e00a4b29df4ce2da5e9a
Alvin Wanjala <40637276+alvyynm@users.noreply.github.com>
2024-07-02 15:20:33 +0300

    Fixes "unexpected application error" thrown in the shop/item/itemID  and shop/checkout pages (#206)
    * feat: update mutations to tanstack-query v5 syntax
    
    * feat: add id param to queryKey array
    
    * feat: update mutations to tanstack-query v5 syntax
    
    * feat: add id param to queryKey array

--- src/hooks/Mutations/shop/useCartSwagg.jsx
+++ src/hooks/Mutations/shop/useCartSwagg.jsx
@@ -7,8 +7,8 @@ const useAddSwagToCart = () => {
   const { auth, logout } = useAuth();
   const queryClient = useQueryClient();
 
-  return useMutation(
-    async (cartItems) => {
+  return useMutation({
+    mutationFn: async (cartItems) => {
       const response = await publicAxios.post("/cart/swaggs/", cartItems, {
         headers: {
           "Content-Type": "application/json",
@@ -17,28 +17,26 @@ const useAddSwagToCart = () => {
       });
       return response.data;
     },
-    {
-      onSuccess: (data) => {
-        console.log("Added to cart: ", data);
-        queryClient.invalidateQueries(["productsInCart"]);
-      },
-      onError: (error) => {
-        // eslint-disable-next-line no-console
-        console.error("Unable to add availability");
-        if (error.response.status === 401) {
-          logout();
-        }
-      },
-    }
-  );
+    onSuccess: (data) => {
+      console.log("Added to cart: ", data);
+      queryClient.invalidateQueries({ queryKey: ["productsInCart"] });
+    },
+    onError: (error) => {
+      // eslint-disable-next-line no-console
+      console.error("Unable to add availability");
+      if (error.response.status === 401) {
+        logout();
+      }
+    },
+  });
 };
 
 const useDeleteSwag = () => {
   const { auth, logout } = useAuth();
   const queryClient = useQueryClient();
 
-  return useMutation(
-    async (id) => {
+  return useMutation({
+    mutationFn: async (id) => {
       const response = await publicAxios.delete(`/cart/swaggs/${id}/`, {
         headers: {
           "Content-Type": "application/json",
@@ -48,20 +46,18 @@ const useDeleteSwag = () => {
       console.log("response ", response.data);
       return response.data;
     },
-    {
-      onSuccess: (data) => {
-        console.log("Successfully deleted ", data);
-        queryClient.invalidateQueries(["productsInCart"]);
-      },
-      onError: (error) => {
-        // eslint-disable-next-line no-console
-        console.error("Unable to delete swag");
-        if (error.response.status === 401) {
-          logout();
-        }
-      },
-    }
-  );
+    onSuccess: (data) => {
+      console.log("Successfully deleted ", data);
+      queryClient.invalidateQueries({ queryKey: ["productsInCart"] });
+    },
+    onError: (error) => {
+      // eslint-disable-next-line no-console
+      console.error("Unable to delete swag");
+      if (error.response.status === 401) {
+        logout();
+      }
+    },
+  });
 };
 
 export { useAddSwagToCart, useDeleteSwag };
