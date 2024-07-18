48c7940562ab99a3d158beb3bbb35d9c26d14ec1
mingabire809 <57630708+mingabire809@users.noreply.github.com>
2024-07-18 16:38:44 +0300

    removing duplicate files (#185)
    * removing duplicate files
    
    * refactor: returned missing files
    
    ---------
    
    Co-authored-by: felixNyalenda <felixasejunior@gmail.com>
    Co-authored-by: Sonia Lomo <49971500+sonylomo@users.noreply.github.com>
    Co-authored-by: sonylomo <sonylomo1@gmail.com>

--- src/assets/images/podcast/index.js
+++ src/assets/images/podcast/index.js
@@ -1,21 +1,21 @@
+import Balogun from "./balogun.png";
+import Jean from "./jean_opiyo.png";
+import Kristin from "./kristin.png";
+import Melody from "./melody.png";
 import podImage1 from "./podImage1.png";
 import podImage2 from "./podImage2.png";
 import RichardMulandi from "./richard_mulandi.png";
-import Balogun from "./balogun.png";
 import Saruni from "./saruni.png";
-import Jean from "./jean_opiyo.png";
 import Vivian from "./vivian.png";
-import Melody from "./melody.png";
-import Kristin from "./kristin.png";
 
 export {
+  Balogun,
+  Jean,
+  Kristin,
+  Melody,
+  RichardMulandi,
+  Saruni,
+  Vivian,
   podImage1,
   podImage2,
-  RichardMulandi,
-  Balogun,
-  Saruni,
-  Jean,
-  Vivian,
-  Melody,
-  Kristin,
 };
