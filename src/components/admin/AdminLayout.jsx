7ce4b3a344b77d0a11fa20fb0e76761990fe8ac5
sonylomo <sonylomo1@gmail.com>
2024-08-08 13:13:29 +0300

    fix: lint errror

--- src/components/admin/AdminLayout.jsx
+++ src/components/admin/AdminLayout.jsx
@@ -1,7 +1,8 @@
 import { Outlet } from "react-router-dom";
 import AdminHeader from "./AdminHeader";
-import BottomNavBar from "./BottomNavBar";
 import AdminMain from "./AdminMain";
+import BottomNavBar from "./BottomNavBar";
+
 function AdminLayout() {
   return (
     <div className="w-full overflow-hidden">
