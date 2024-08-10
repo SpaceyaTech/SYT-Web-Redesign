6a9527dd792af699dc86c290e4f03648ebe03efa
alvyynm <alvyynm@gmail.com>
2024-08-10 14:58:27 +0300

    feat: update api endpoint for useMakeOrder mutation hook

--- src/hooks/Mutations/shop/useMakeOrder.jsx
+++ src/hooks/Mutations/shop/useMakeOrder.jsx
@@ -2,17 +2,17 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import privateAxios from "../../../api/privateAxios";
 import useAuth from "../../useAuth";
 
-// POST: https://apis.spaceyatech.com/api/orders/
+// POST: https://apis.spaceyatech.com/api/checkout/
 const useMakeOrder = () => {
   const { auth, logout } = useAuth();
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: async (customerInfo) => {
-      const response = await privateAxios.post("/orders/", customerInfo, {
+      const response = await privateAxios.post("/checkout/", customerInfo, {
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer ${auth?.access}`,
+          // Authorization: `Bearer ${auth?.access}`,
         },
       });
       return response.data;
