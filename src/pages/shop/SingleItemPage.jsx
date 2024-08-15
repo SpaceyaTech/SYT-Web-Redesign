ba0030e3d95c32b21d69c2fac32d8043a6241a64
alvyynm <alvyynm@gmail.com>
2024-08-15 06:22:46 +0300

    fix: item count update bug

--- src/pages/shop/SingleItemPage.jsx
+++ src/pages/shop/SingleItemPage.jsx
@@ -144,6 +144,14 @@ export default function SingleItemPage() {
   //   navigate("/shop/checkout");
   // };
 
+  const increment = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  const decrement = () => {
+    setCount((prevCount) => (prevCount > 1 ? prevCount - 1 : prevCount));
+  };
+
   const getSizeKeyByValue = (value) =>
     Object.keys(sizes).find((key) => sizes[key] === value);
 
@@ -275,12 +283,40 @@ export default function SingleItemPage() {
               <p className="font-normal text-red-400 text-center">{error}</p>
             )}
             <div className="flex justify-start gap-3 ">
-              <Counter
+              {/* <Counter
                 className="h-12 sm:h-14 w-32"
                 count={count}
                 setCount={setCount}
                 maxStock={selectedVariant?.stock}
-              />
+              /> */}
+              <div className="flex rounded-lg h-12 sm:h-14 w-32">
+                <button
+                  type="button"
+                  data-action="decrement"
+                  className="cursor-pointer outline-none w-20 border-y border-l border-l-[#EAECF0] border-y-[#EAECF0] rounded-l-md border-r"
+                  onClick={decrement}
+                >
+                  <span className=" text-base">−</span>
+                </button>
+                <p
+                  className="outline-none focus:outline-none font-medium  md:text-base cursor-default flex items-center justify-center border-y px-5 border-y-[#EAECF0]"
+                  name="custom-input-number"
+                >
+                  {count}
+                </p>
+                <button
+                  disabled={
+                    selectedVariant?.stock === count ||
+                    count > selectedVariant?.stock
+                  }
+                  type="button"
+                  data-action="increment"
+                  className="cursor-pointer outline-none w-20 border-y border-r border-r-[#EAECF0] border-y-[#EAECF0] rounded-r-md border-l"
+                  onClick={increment}
+                >
+                  <span className="text-base">+</span>
+                </button>
+              </div>
 
               <button
                 type="button"
