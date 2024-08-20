258c157e555e79ec9993db14fa41c4317c0068dc
alvyynm <alvyynm@gmail.com>
2024-08-20 23:14:37 +0300

    Revert "chore: fix vitest config to check for all .test.js files"
    This reverts commit aaca2133e34e05df3b11f664b0d376971aa3e49f.
    
    revert aaca213

--- vite.config.js
+++ vite.config.js
@@ -21,13 +21,7 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [million.vite({ auto: true }), react()],
     test: {
-      include: ["**/*.test.js"],
-      exclude: [
-        "**/node_modules/**",
-        "**/dist/**",
-        "**/.{idea,git,cache,output,temp}/**",
-        "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
-      ],
+      include: ["src/**/*.test.js"],
     },
     server: {
       watch: {
