7728da9383e6b3a19d4bd2f1fc218e0192579924
alvyynm <alvyynm@gmail.com>
2024-08-21 10:49:35 +0300

    Revert "fix: try addressing deployment issue"
    This reverts commit ffab5d80f7f30e413e64ced8f5a924cf77de42f5.
    
    Revert ffab5d8

--- vite.config.js
+++ vite.config.js
@@ -29,7 +29,7 @@ export default defineConfig(({ mode }) => {
         "**/.{idea,git,cache,output,temp}/**",
         "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
       ],
-      // globals: true,
+      globals: true,
     },
     server: {
       watch: {
