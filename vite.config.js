78d77519efd0af266e641c295b1c66526a46c3ba
alvyynm <alvyynm@gmail.com>
2024-08-21 10:55:20 +0300

    Revert "fix: try addressing deployment issue"
    This reverts commit 241ac5dedb6cf2eade791f63aa089b76f5799190.
    
    Revert 241ac5d

--- vite.config.js
+++ vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig(({ mode }) => {
     plugins: [million.vite({ auto: true }), react()],
     test: {
       environment: "jsdom",
-      include: ["**/*.test.js", "**/*.test.jsx"],
+      include: ["**/*.test.[jt]s?(x)"],
       exclude: [
         "**/node_modules/**",
         "**/dist/**",
