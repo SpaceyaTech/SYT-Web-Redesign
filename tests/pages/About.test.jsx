b212a56cc1cdb22966655c6efc97a1b06b581456
alvyynm <alvyynm@gmail.com>
2024-10-22 12:16:30 +0300

    feat(test): write test for successful render of About us page

---
+++ tests/pages/About.test.jsx
@@ -0,0 +1,21 @@
+import { render, screen } from "@testing-library/react";
+import { HelmetProvider } from "react-helmet-async";
+import { BrowserRouter } from "react-router-dom";
+import { describe, expect, it } from "vitest";
+
+import AboutUs from "../../src/pages/aboutUs/AboutUs";
+
+describe("About us page unit tests", () => {
+  const renderWithRouter = (ui) =>
+    render(
+      <HelmetProvider>
+        <BrowserRouter>{ui}</BrowserRouter>
+      </HelmetProvider>
+    );
+
+  it("should render About Us page successfully", () => {
+    renderWithRouter(<AboutUs />);
+    const titleElement = screen.getByTestId("title");
+    expect(titleElement).toBeTruthy();
+  });
+});
