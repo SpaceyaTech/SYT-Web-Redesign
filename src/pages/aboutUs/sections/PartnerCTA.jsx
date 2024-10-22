a1ba1eefaaa7a30ac6909ac77374ecf3c5b2417c
alvyynm <alvyynm@gmail.com>
2024-10-22 12:31:18 +0300

    feat(test): define data-testid for reports section

--- src/pages/aboutUs/sections/PartnerCTA.jsx
+++ src/pages/aboutUs/sections/PartnerCTA.jsx
@@ -5,7 +5,12 @@ function PartnerCTA() {
     <section className="pt-16 pb-10 mx-auto w-full max-w-screen-2xl">
       <div className="flex flex-col sm:flex-row justify-between px-4 sm:px-28">
         <div className="space-y-4 mb-8 sm:mb-0">
-          <h3 className="font-semibold text-md sm:text-xl">Our Reports</h3>
+          <h3
+            className="font-semibold text-md sm:text-xl"
+            data-testid="reports"
+          >
+            Our Reports
+          </h3>
           <p>
             <a
               href={Report}
