263150cc8f1afcdbbaa562cccbebd36eff852dde
alvyynm <alvyynm@gmail.com>
2024-10-29 12:32:01 +0300

    fix: add event URL to events

--- src/pages/community/sections/eventsPreview/SingleEvents/sections/Hero.jsx
+++ src/pages/community/sections/eventsPreview/SingleEvents/sections/Hero.jsx
@@ -180,17 +180,19 @@ ${isVirtual ? "text-white" : "text-green-header"}
             </div>
           </div>
 
-          <button
-            type="button"
-            onClick={openModal}
+          <a
+            href={event?.link}
+            target="_blank"
+            rel="noreferrer"
+            // onClick={openModal}
             className={`py-2 px-20 rounded-lg ${
               isVirtual
                 ? "text-green-header bg-white"
                 : "text-white bg-gradient-to-b to-primary from-green-dark"
             }`}
           >
-            Reserve for Free
-          </button>
+            Reserve Tickets
+          </a>
         </div>
 
         <EventRSVP
