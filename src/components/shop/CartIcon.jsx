9e30747510424da7bbaf684dbcd8acab4f556095
alvyynm <alvyynm@gmail.com>
2024-10-29 15:33:27 +0300

    feat: display cart items count besides the cart icon

--- src/components/shop/CartIcon.jsx
+++ src/components/shop/CartIcon.jsx
@@ -4,10 +4,13 @@ import SectionWrapper from "./SectionWrapper";
 function CartIcon() {
   return (
     <SectionWrapper>
-      <div className="flex justify-end   ">
+      <div className="flex justify-end relative">
         <div className="w-12 md:w-16 h-12 md:h-16 rounded-full p-0.5 md:p-1  bg-white border shadow-lg cursor-pointer">
           <div className="flex w-full h-full p-1.5 md:p-2 rounded-full justify-center items-center bg-green-dark">
             <MdAddShoppingCart color="white" className="h-full w-full" />
+          </div>
+          <div className="absolute bottom-0 right-0 bg-[#B3261E] text-white text-xs font-bold rounded-full w-5 h-5 flex items-center justify-center">
+            <p className="text-white font-medium">1</p>
           </div>
         </div>
       </div>
