308c70adcfe3ba2efd861017f1979a4abb4b276a
alvyynm <alvyynm@gmail.com>
2024-10-30 13:29:16 +0300

    feat: properly format event descriptions

--- src/pages/community/sections/eventsPreview/SingleEvents/sections/EventDescription.jsx
+++ src/pages/community/sections/eventsPreview/SingleEvents/sections/EventDescription.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
 import React from "react";
+import formatEventDescription from "../../../../../../utilities/formatEventDescription";
 
 function EventDescription({ eventDesc }) {
   return (
-    <div className="w-full text-center max-w-3xl mx-auto text-sm md:text-[15px] md:leading-[22px] font-normal text-gray-600">
-      {eventDesc}
+    <div className="w-full max-w-3xl mx-auto text-sm md:text-[15px] md:leading-[22px] font-normal text-gray-600">
+      {formatEventDescription(eventDesc)}
     </div>
   );
 }
